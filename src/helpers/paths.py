61fd1922db106d7ddd0e033cc749f48b620fe0af
apauliuc <andupauliuc@gmail.com>
2019-04-12 12:54:30 +0200

    Restructure code part2

---
+++ src/helpers/paths.py
@@ -0,0 +1,40 @@
+import os
+import time
+import datetime
+
+from definitions import DATA_DIR, RUNS_DIR
+
+DEFAULT_NAME = 'Run'
+NAME_TEMPLATE = '_%m-%d_%H-%M-%S'
+
+
+def get_dataset_path(dataset_path=DATA_DIR, dataset_name='AMC', dataset_type='train') -> str:
+    return os.path.join(dataset_path, dataset_name, dataset_type)
+
+
+def create_run_name(name: str, time_signature: float) -> str:
+    return name + datetime.datetime.fromtimestamp(time_signature).strftime(NAME_TEMPLATE)
+
+
+def get_new_run_path(name: str) -> str:
+    if name is None:
+        name = DEFAULT_NAME
+    path = os.path.join(RUNS_DIR, create_run_name(name, time.time()))
+    create_directory(path)
+    return path
+
+
+def get_resume_model_path(run_dir: str, model_filename: str) -> str:
+    model_path = os.path.join(RUNS_DIR, run_dir, f'{model_filename}.pth')
+    return model_path
+
+
+def get_resume_optimizer_path(run_dir: str, optim_filename: str) -> str:
+    optim_path = os.path.join(RUNS_DIR, run_dir, f'{optim_filename}.pth')
+    return optim_path
+
+
+def create_directory(path: str):
+    if path:
+        if not os.path.exists(path):
+            os.makedirs(path)
---
+++ src/helpers/utils.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import logging
+
+
+def setup_logger(logdir):
+    log_format = '[%(levelname)s: %(filename)s: %(lineno)4d]: %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_format, stream=sys.stdout)
+
+    formatter = logging.Formatter(log_format)
+    logger = logging.getLogger('main_scripts')
+    handler = logging.FileHandler(os.path.join(logdir, "LOGGING_FILE.log"))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+    logging.getLogger("ignite.engine.engine.Engine").setLevel(logging.WARNING)
+
+    return logger, handler
+
+
+def dice_coefficient(prediction, target) -> float:
+    smooth = 1e-5
+    batch_size = prediction.shape[0]
+    m1 = prediction.view(batch_size, -1)
+    m2 = target.view(batch_size, -1)
+    score_per_item = (2. * (m1 * m2).sum(dim=1) + smooth) / \
+                     (m1.sum(dim=1) + m2.sum(dim=1) + smooth)
+    return score_per_item.mean()
+
+
+def timer_to_str(value):
+    m, s = divmod(value, 60)
+    h, m = divmod(m, 60)
+    return "%02i:%02i:%02i" % (h, m, s)
