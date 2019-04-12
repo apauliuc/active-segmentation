a4bdad4c8837f04cb0e7e1ca8747215d0bbfda97
apauliuc <andupauliuc@gmail.com>
2019-04-12 17:17:50 +0200

    Added possibility to train multiple config files from directory

--- src/main.py
+++ src/main.py
@@ -7,9 +7,28 @@ from helpers.config import ConfigClass
 from main_scripts.predict import prediction_main
 from main_scripts.trainer import Trainer
 from helpers.paths import get_new_run_path
-from definitions import CONFIG_STANDARD, DATA_DIR, RUNS_DIR
+from definitions import CONFIG_STANDARD, DATA_DIR, RUNS_DIR, CONFIG_DIR
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+
+
+def train_config(args, config_path: str):
+    with open(config_path, 'r') as f:
+        config = ConfigClass(yaml.load(f))
+
+    config.data.mode = 'train'
+    config.data.path = args.ds_path
+    run_dir = get_new_run_path(config.run_name)
+
+    with open(os.path.join(run_dir, 'cfg_file.yml'), 'w+') as f:
+        yaml.dump(config, f)
+
+    trainer = Trainer(config, run_dir)
+    trainer.run()
+
+    if args.train_predict:
+        config.data.mode = 'predict'
+        prediction_main(run_dir_name=run_dir, config=config, name=config.run_name)
 
 
 def main(args):
@@ -18,25 +37,16 @@ def main(args):
         dataset_path = os.path.join(DATA_DIR, 'AMC Dummy')
 
         scan_names = mds_preprocess_scans(data_root_dir, max_clip=255, clip_max_to_0=False)
-        # scan_names = 'arr_scan_eroded_1.npy'
         mds_separate_scans_to_slices(data_root_dir, dataset_path, scan_names, dummy_dataset=True)
 
     elif args.run_type == 'train':
-        with open(args.config, 'r') as f:
-            config = ConfigClass(yaml.load(f))
+        train_config(args, args.config)
 
-        config.data.mode = 'train'
-        config.data.path = args.ds_path
-        run_dir = get_new_run_path(config.run_name)
+    elif args.run_type == 'train_all_configs':
+        configs_list = [os.path.join(args.configs_dir, x) for x in os.listdir(args.configs_dir)]
 
-        with open(os.path.join(run_dir, 'cfg_file.yml'), 'w+') as f:
-            yaml.dump(config, f)
-
-        Trainer(config, run_dir).run()
-
-        if args.train_predict:
-            config.data.mode = 'predict'
-            prediction_main(run_dir_name=run_dir, config=config, name=config.run_name)
+        for config in configs_list:
+            train_config(args, config)
 
     elif args.run_type == 'predict':
         with open(os.path.join(RUNS_DIR, args.run_dir, 'cfg_file.yml')) as f:
@@ -49,16 +59,18 @@ def main(args):
 if __name__ == '__main__':
     "Main starting point of the application"
     parser = argparse.ArgumentParser()
-    parser.add_argument('--run_type', type=str, default='train',
-                        help='Type of run', choices=['train', 'predict', 'preprocess'])
+    parser.add_argument('--run_type', type=str, default='train_all_configs',
+                        help='Type of run', choices=['train', 'predict', 'preprocess', 'train_all_configs'])
     parser.add_argument('--config', type=str, default=CONFIG_STANDARD,
                         help='Configuration file to use')
+    parser.add_argument('--configs_dir', type=str, default=CONFIG_DIR,
+                        help='Directory of all configurations to train on')
     parser.add_argument('--ds_path', type=str, default=DATA_DIR,
                         help='Path to main data directory')
     parser.add_argument('--train_predict', type=bool, default=True,
                         help='Indicate whether to predict after training is finished')
     parser.add_argument('--run_dir', type=str, default='',
-                        help='Previous run directory to load model from (works only for predict run type)')
+                        help='Previous run directory to load model from (works only for run_type = predict)')
 
     arguments = parser.parse_args()
     main(arguments)
