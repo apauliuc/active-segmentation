3e194487ebf2b6e3dbb9f8e89f203e916022b723
apauliuc <andupauliuc@gmail.com>
2019-06-06 20:43:12 +0200

    Changed definitions file

--- src/definitions.py
+++ src/definitions.py
@@ -10,11 +10,16 @@ CONFIG_DEFAULT = os.path.join(CONFIG_DIR, 'default_config.yml')
 CONFIG_AL = os.path.join(CONFIG_DIR, 'al_config.yml')
 CONFIG_VOC = os.path.join(CONFIG_DIR, 'voc_config.yml')
 
-if os.environ.get('USER') == 'pauliuca' or os.environ.get('USER') == 'andrei':
+if os.environ.get('USER') in ['pauliuca', 'andrei']:
     # SURFSARA OR MAC
     RUNS_DIR = os.path.join(ROOT_DIR, 'runs')
     DATA_DIR = os.path.join(ROOT_DIR, 'data')
+elif os.environ.get('USER') == 'apauliuc':
+    # DAS4
+    RUNS_DIR = os.path.join('/var', 'scratch', 'apauliuc', 'runs')
+    DATA_DIR = os.path.join('/var', 'scratch', 'apauliuc', 'data')
 elif os.environ.get('USERNAME') == 'aspauliuc':
+    # AMC
     RUNS_DIR = os.path.join('C:', 'Andrei', 'RUNS')
     DATA_DIR = os.path.join('C:', 'Andrei', 'data')
 
